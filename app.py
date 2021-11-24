9cbd445fd35c0d47ec54754e37fa24e9372da9d0
Vikram Singh <vsingh18567@gmail.com>
2021-11-23 21:17:23 -0500

    some linting and touch ups to prepare for testing

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, url_for, redirect
-import requests
-from dotenv import load_dotenv
-import json
 import os
+
+import requests
 from dateutil import parser
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, url_for
 
 # consts
 load_dotenv()
@@ -13,28 +13,39 @@ API_EMAIL = os.environ.get("EMAIL")
 API_TOKEN = os.environ.get("TOKEN")
 
 
+def prettify_ticket(ticket):
+    """
+    makes ticket info pretty
+    """
+    ticket_time = parser.parse(ticket["created_at"])
+    ticket["created_at"] = f"{ticket_time.year}/{ticket_time.month}/{ticket_time.day}"
+
+    ticket["status"] = str(ticket["status"]).capitalize()
+    ticket["priority"] = str(ticket["priority"]).capitalize()
+    return ticket
+
+
 @app.route("/tickets", methods=["GET", "POST"])
 def tickets():
     """
     route for viewing all tickets
     """
     if request.method == "GET":
-        request_url = request.args.get("request_url", default=f"{main_url}tickets.json?page[size]=25", type=str)
+        request_url = request.args.get(
+            "request_url", default=f"{main_url}tickets.json?page[size]=25", type=str
+        )
         page = request.args.get("page_num", default=1, type=int)
         error = request.args.get("error", default=None, type=str)
-
         response = requests.get(request_url, auth=(f"{API_EMAIL}/token", API_TOKEN))
+
         if response.status_code > 299:
+            if request_url != f"{main_url}tickets.json?page[size]=25":
+                # try hitting the default page
+                return redirect(url_for("tickets", error="Oops something went wrong"))
+            # otherwise its probably some server error
             return render_template("error.html")
+
         data = response.json()
-        if len(data["tickets"]) == 0:
-            error = "Oops something went wrong"
-            request_url = f"{main_url}tickets.json?page[size]=25"
-            page = 1
-            response = requests.get(request_url, auth=(f"{API_EMAIL}/token", API_TOKEN))
-            if response.status_code > 299:
-                return render_template("error.html")
-            data = response.json()
 
         next_link = None
         prev_link = data["links"]["prev"]
@@ -44,9 +55,10 @@ def tickets():
 
         if page == 1:
             prev_link = None
+
         for ticket in data["tickets"]:
-            ticket_time = parser.parse(ticket["created_at"])
-            ticket["created_at"] = f"{ticket_time.year}/{ticket_time.month}/{ticket_time.day}"
+            # make dates pretty
+            ticket = prettify_ticket(ticket)
 
         return render_template(
             "view_all.html",
@@ -54,52 +66,66 @@ def tickets():
             next_link=next_link,
             prev_link=prev_link,
             error=error,
-            page=page
+            page=page,
         )
     else:
         id = request.form["ticket_id"]
-        return redirect(url_for('ticket_id', id=id))
+        return redirect(url_for("ticket_id", id=id))
 
 
-@app.route("/ticket/<id>")
+@app.route("/ticket/<id>", methods=["GET"])
 def ticket_id(id):
     """
     :param id: ticket id
     route for viewing details of a specific ticket
     """
-    response = requests.get(f"{main_url}tickets/{id}.json", auth=(f"{API_EMAIL}/token", API_TOKEN))
+    response = requests.get(
+        f"{main_url}tickets/{id}.json", auth=(f"{API_EMAIL}/token", API_TOKEN)
+    )
     if response.status_code == 200:
         ticket = response.json()["ticket"]
-        '''
+        """
         the relevant data:
             id, subject, description, priority, status, time created, requester_id,
             submitter_id, organization_id
-        '''
-        ticket_time = parser.parse(ticket["created_at"])
-        ticket["created_at"] = f"{ticket_time.year}/{ticket_time.month}/{ticket_time.day}"
-        org_response = requests.get(f"{main_url}organizations/{ticket['organization_id']}.json",
-                                    auth=(f"{API_EMAIL}/token", API_TOKEN))
+        """
+        # prettify
+        ticket = prettify_ticket(ticket)
+
+        org_response = requests.get(
+            f"{main_url}organizations/{ticket['organization_id']}.json",
+            auth=(f"{API_EMAIL}/token", API_TOKEN),
+        )
         org = None
         if org_response.status_code == 200:
             print(org_response.json())
             org = org_response.json()["organization"]["name"]
-        req_response = requests.get(f"{main_url}users/{ticket['requester_id']}.json",
-                                    auth=(f"{API_EMAIL}/token", API_TOKEN))
+        req_response = requests.get(
+            f"{main_url}users/{ticket['requester_id']}.json",
+            auth=(f"{API_EMAIL}/token", API_TOKEN),
+        )
         req = None
         if req_response.status_code == 200:
             print(req_response.json())
             req = req_response.json()["user"]["name"]
-        sub_response = requests.get(f"{main_url}users/{ticket['submitter_id']}.json",
-                                    auth=(f"{API_EMAIL}/token", API_TOKEN))
+        sub_response = requests.get(
+            f"{main_url}users/{ticket['submitter_id']}.json",
+            auth=(f"{API_EMAIL}/token", API_TOKEN),
+        )
         sub = None
         if sub_response.status_code == 200:
             sub = sub_response.json()["user"]["name"]
-        return render_template("view_one.html", ticket=ticket, org=org, req=req, sub=sub)
+        return render_template(
+            "view_one.html", ticket=ticket, org=org, req=req, sub=sub
+        )
     else:
         text = response.json()
         if text.get("error") == "RecordNotFound":
-            return redirect(url_for('tickets', error="Could not find ticket with that ID"))
+            return redirect(
+                url_for("tickets", error="Could not find ticket with that ID")
+            )
         return render_template("error.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=8000)
